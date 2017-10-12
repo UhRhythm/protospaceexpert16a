0fdbc89b2e25033fb307d3f2b228b192aadc755d
UhRhythm <uhrhythm.0@gmail.com>
2017-10-13 06:44:40 +0900

    comit

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,7 +1,6 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :captured_images, dependent: :destroy
-  has_many :comments
 
   accepts_nested_attributes_for :captured_images, reject_if: :reject_sub_images
 
@@ -15,10 +14,11 @@ class Prototype < ActiveRecord::Base
   end
 
   def set_main_thumbnail
-    captured_images.main.first.content
+    captured_images.main.last.content
   end
 
   def posted_date
     created_at.strftime('%b %d %a')
   end
 end
+
