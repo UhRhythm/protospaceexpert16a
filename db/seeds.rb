c0caff8390f3bca53fec38227a47cf08db8b9e82
UhRhythm <uhrhythm.0@gmail.com>
2017-10-13 10:06:37 +0900

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@ require "csv"
 companies_csv = CSV.readlines("db/comments.csv")
 companies_csv.shift
 companies_csv.each do |row|
-  Comment.create(text: row[1], user_id: row[2], prototype_id: row[3], user_id: row[4])
+  Comment.create(text: row[1], user_id: row[2], prototype_id: row[3])
 end
