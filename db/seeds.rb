504af4bac1958389910ef3703fc43e356d31d374
UhRhythm <uhrhythm.0@gmail.com>
2017-10-13 09:58:57 +0900

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
 # encoding: utf-8
 require "csv"
-
 companies_csv = CSV.readlines("db/prototypes.csv")
 companies_csv.shift
 companies_csv.each do |row|
-  Company.create(title: row[1], catch_copy: row[2], concept: row[3], user_id: row[4])
+  Prototype.create(title: row[1], catch_copy: row[2], concept: row[3], user_id: row[4])
 end
-
