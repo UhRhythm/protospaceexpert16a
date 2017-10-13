909b7300cf3618279453c0ef0bbe0ff0f404938b
UhRhythm <uhrhythm.0@gmail.com>
2017-10-13 09:28:45 +0900

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-prototype = Prototype.new(title: "test", catch_copy: "test", concept: "test", user_id: 1)
+prototype = Prototype.new(title: "test", catch_copy: "tested", concept: "test", user_id: 1)
 prototype.save!
