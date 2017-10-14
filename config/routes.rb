4efdd00e000adecd46c60aed8ac60b778ada1251
UhRhythm <uhrhythm.0@gmail.com>
2017-10-14 10:27:29 +0900

    Initialize repository

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :prototypes do
     resources :comments, only: [:new, :create, :edit, :update, :destroy]
   end
+  resources :prototypes
   resources :users, only: [:show, :edit, :update]
 end
