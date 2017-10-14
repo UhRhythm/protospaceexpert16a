735177a7031211b2bd293b4b0052e8475e849c81
UhRhythm <uhrhythm.0@gmail.com>
2017-10-14 10:11:16 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-
-  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update]
+  resources :prototypes, only: [:index, :new, :create, :show] do
+    resources :comments, only: [:new, :create, :edit, :update, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
 end
-
