7bcbdaa9bcf73c1829a47f495410ddd6e6f497cb
UhRhythm <uhrhythm.0@gmail.com>
2017-10-19 10:54:07 +0900

    comment out script.js

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -29,32 +29,32 @@
 //   })
 // });
 
-$(function(){
-  function buildHTML(comment){
-    var html = `<h4>${comment.user.name}</h4>
-                <p>${comment.text}</p>
-               `
-    return html;
-  }
-  $('#new_comment').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: 'POST',
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.medie-body').append(html)
-      $('.text-box').val('')
-    })
-    .fail(function(){
-      alert('error');
-    })
-  })
-});
+// $(function(){
+//   function buildHTML(comment){
+//     var html = `<h4>${comment.user.name}</h4>
+//                 <p>${comment.text}</p>
+//                `
+//     return html;
+//   }
+//   $('#new_comment').on('submit', function(e){
+//     e.preventDefault();
+//     var formData = new FormData(this);
+//     var url = $(this).attr('action')
+//     $.ajax({
+//       url: url,
+//       type: 'POST',
+//       data: formData,
+//       dataType: 'json',
+//       processData: false,
+//       contentType: false
+//     })
+//     .done(function(data){
+//       var html = buildHTML(data);
+//       $('.medie-body').append(html)
+//       $('.text-box').val('')
+//     })
+//     .fail(function(){
+//       alert('error');
+//     })
+//   })
+// });
