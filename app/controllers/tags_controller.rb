b0abf7deed570c679bfb40fd181767a3e2aada60
shwan01 <tenori6452@gmail.com>
2017-10-24 11:06:27 +0900

    タグのshowアクションの定義とビュー作成

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,8 @@
 class TagsController < ApplicationController
 
   def show
-
+    @tag = Tag.find(params[:id])
+    @prototypes = Prototype.includes(:tags).where(tags: {id: (params[:id])})
   end
 
 end
