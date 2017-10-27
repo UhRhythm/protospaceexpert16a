6e5c85b941e07d0bb587acce1916b0e121848076
shwan01 <tenori6452@gmail.com>
2017-10-27 11:23:54 +0900

    グルーピングして表示

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,12 +1,12 @@
 class TagsController < ApplicationController
 
   def index
-    @tags = Tag.all
+    @tags = Tag.group(:name)
   end
 
   def show
     @tag = Tag.find(params[:id])
-    @prototypes = Prototype.includes(:tags).where(tags: {id: (params[:id])})
+    @prototypes = Prototype.includes(:tags).where(tags: {name: @tag.name})
     @tags = Tag.group(:name).count.find(params[:id])
   end
 
